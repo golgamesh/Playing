845b51a7fd427260893be03b08316298afa7adcf
Mark Gabriel <golgamesh01@hotmail.com>
2018-05-13 23:25:02 -0400

    ere

--- reactjs-basics-master/src/app/components/home.js
+++ reactjs-basics-master/src/app/components/home.js
@@ -2,11 +2,27 @@ import React from 'react';
 
 export class Home extends React.Component {
     render() {
+        var text = "something";
+        console.log(this.props);
         return (
             <div>
                 <p>In a new component</p>
-                <div></div>
+                <div>Your name is {this.props.name}, your age is {this.props.age}</div>
+                <div>
+                    <ul>
+                        {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
+                    </ul>
+                </div>
+                <hr/>
+                {this.props.children}
             </div>
         );
     }
 }
+
+Home.propTypes = {
+    name: React.propTypes.string,
+    age: React.propTypes.number,
+    user: React.propTypes.object,
+    children: React.propTypes.required
+};
--- reactjs-basics-master/src/app/index.jsx
+++ reactjs-basics-master/src/app/index.jsx
@@ -5,6 +5,11 @@ import { Home } from "./components/home";
 
 class App extends React.Component {
     render() {
+        var user = {
+            name: "Max",
+            age: 9,
+            hobbies: ["sports", "foosball", "video games"]
+        }
         return (
             <div className="container">
                 <div className="row">
@@ -14,7 +19,9 @@ class App extends React.Component {
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home/>
+                        <Home name={"Max"} age={9} user={user}>
+                            <p>This is a paragraph.</p>
+                        </Home>
                     </div>
                 </div>
             </div>
