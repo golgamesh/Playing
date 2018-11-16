f00973387bb1a290366c4248099c768764d38838
Mark Gabriel <golgamesh01@hotmail.com>
2018-11-16 00:59:57 -0500

    commented dropdownresettable

--- react-advanced-search-webpart/src/webparts/advancedSearch/components/DropdownResettable.tsx
+++ react-advanced-search-webpart/src/webparts/advancedSearch/components/DropdownResettable.tsx
@@ -7,36 +7,55 @@ import {
     IDropdownProps
 } from 'office-ui-fabric-react/lib/Dropdown';
 
+/**
+ * This component's property interface
+ */
 export interface IDropdownResettableProps extends IDropdownProps {
     options: Array<IDropdownResettableOption>
 }
 
+/**
+ * This component's state interface
+ */
 export interface IDropdownResettableState {
-    // options: Array<IDropdownResettableOption>
     selectedKey?: number | string;
 }
 
+/**
+ * Dropdown option extension
+ */
 export interface IDropdownResettableOption extends IDropdownOption {
     value?: number | string;
 }
 
+/**
+ * DropdownResettable React Component Class definition
+ */
 export default class DropdownResettable extends React.Component<IDropdownResettableProps, {}> {
     constructor(props: IDropdownResettableProps) {
         super(props);
 
-        this.state = {
-            //options: [...props.options],
+        this.state = {                                                      // Init state object
             selectedKey: props.selectedKey
         };
 
-        this.options = this._includeResetDropdownChoice();
+        this.options = this._includeResetDropdownChoice();                  // build dropdown options
 
     }
 
+    /**
+     * Dropdown options array
+     */
     public options: IDropdownResettableOption[];
-    //public selectedKey?: number | string;
+
+    /**
+     * React component's state object
+     */
     public state: IDropdownResettableState;
 
+    /**
+     * React component's render method
+     */
     public render(): React.ReactElement<IDropdownResettableProps> {
         return(
             <Dropdown {...this.props } 
@@ -47,84 +66,67 @@ export default class DropdownResettable extends React.Component<IDropdownResetta
         );
     }
 
+    /**
+     * React Component's will receive props event handler
+     * @param nextProps 
+     */
     public componentWillReceiveProps(nextProps: IDropdownResettableProps): void {
-        this.setState({
+        this.setState({                                                     // Record new selectedKey from props to state
             ...this.state,
             selectedKey: nextProps.selectedKey
         } as IDropdownResettableState);
     }
-
-    /* 
-    private _includeResetDropdownChoice(field: Model.ISearchProperty, include: boolean): Array<Model.ISearchPropertyChoice> {
-
-        
-        let choices = [...field.options.choices] as Array<Model.ISearchPropertyChoice>;
-
-        if(include) {
-            if(!this._hasDropdownResetChoice(field)) {
-                const resetChoice: Model.ISearchPropertyChoice = {
-                    key: `${field.property}-reset`,
-                    text: '',
-                    value: null
-                };
-                choices.unshift(resetChoice);
-            }
-        } else {
-            if(this._hasDropdownResetChoice(field)) {
-                choices.shift();
-            }
-        }
-
-        return choices;
-    } */
     
-     protected ctrl_changed(selectedOption: IDropdownResettableOption, index: number): void {
-        console.log(selectedOption, index);
+    /**
+     * Dropdown changed event handler
+     * @param selectedOption 
+     * @param index 
+     */
+    protected ctrl_changed(selectedOption: IDropdownResettableOption, index: number): void {
 
-
-        if(selectedOption.value === null) {
-            this.setState({
+        if(selectedOption.value === null) {                                 // If value is null 
+            this.setState({                                                 // reset dropdown to unselected
                 ...this.state,
                 selectedKey: null
             } as IDropdownResettableState,
             () => this._changed(selectedOption, index));
-        } else {
-            this.setState({
+        } else {                                                            // otherwise 
+            this.setState({                                                 // set selectedKey
                 ...this.state,
                 selectedKey: selectedOption.key
             }as IDropdownResettableState,
-            () => this._changed(selectedOption, index));
+            () => this._changed(selectedOption, index));                    // Pass value to parent component change handler
         }
         
     }
 
+    /**
+     * Calls parent components event handler if non-null
+     * @param selectedOption 
+     * @param index 
+     */
     protected _changed(selectedOption: IDropdownResettableOption, index: number) {
-        if(this.props.onChanged) {
-            this.props.onChanged(selectedOption, index);
+        if(this.props.onChanged) {                                          // If onChanged is provied
+            this.props.onChanged(selectedOption, index);                    // call handler
         }
     }
     
+    /**
+     * Prepends an empty choice to the options array for resetting the control
+     */
     private _includeResetDropdownChoice(): Array<IDropdownResettableOption> {
 
         let choices = [...this.props.options] as Array<IDropdownResettableOption>;
 
-        const resetChoice: IDropdownResettableOption = {
+        const resetChoice: IDropdownResettableOption = {                    // Reset options definition
             key: `field-reset`,
             text: '',
             value: null
         };
-        choices.unshift(resetChoice);
 
-        return choices;
-    }
-/* 
-    private _hasDropdownResetChoice(): boolean {
-        return this._hasChoices() && 
-            (this.state.options[0] as IDropdownResettableOption).value === null;
-    }
+        choices.unshift(resetChoice);                                       // Add reset option to beginning of array
 
-    private _hasChoices(): boolean {
-        return this.state.options && this.state.options.length > 0;
-    }  */
+        return choices;                                                     // return options array
+    }
 
 }
